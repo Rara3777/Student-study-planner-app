b8d9e2f92d5ce41750b28aee1e9e8573369fbf06
Rara3777 <ratneshwarveerappan@gmail.com>
2026-04-10 15:47:11 -0400

    Refactor menu function and update task options
    i added this because previously the proirity function wasn't working

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-from tasks import add_task, view_tasks, mark_done, load_tasks, save_tasks, delete_task, prioritize_tasks
+ffrom tasks import add_task, view_tasks, mark_done, load_tasks, save_tasks, prioritize_tasks ,delete_task
 
-tasks = load_tasks()  
+def menu():
+    tasks = load_tasks()
 
-while True:
-    print("\n--- Welcome to Student Study Planner ---")
-    print("1. Add Task")
-    print("2. View Tasks")
-    print("3. Mark Task as Done")
-    print("4. Priortize your task")
-    print("5. Delete tasks")
-    print("6. Exit")
+    while True:
+        print("\n--- Study Planner ---")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Mark Task as Done")
+        print("4. Delete Task")
+        print("5. Change Priority")
+        print("6. Save & Exit")
 
-    choice = input("Enter choice: ")
+        choice = input("Choose an option: ")
 
-    if choice == "1":
-        add_task(tasks)
-    elif choice == "2":
-        view_tasks(tasks)
-    elif choice == "3":
-        mark_done(tasks)
-    elif choice == "4":
-        prioritize_tasks(tasks)
-    elif choice == "5":
-        delete_task(tasks)
-    elif choice == "6":
-        save_tasks(tasks)
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice, try again.")
+        if choice == "1":
+            add_task(tasks)
+        elif choice == "2":
+            view_tasks(tasks)
+        elif choice == "3":
+            mark_done(tasks)
+        elif choice == "4":
+            delete_task(tasks)
+        elif choice == "5":
+            prioritize_tasks(tasks)
+        elif choice == "6":
+            save_tasks(tasks)
+            print("Saved. Goodbye!")
+            break
+        else:
+            print("Invalid choice.")
+
+
+menu()
